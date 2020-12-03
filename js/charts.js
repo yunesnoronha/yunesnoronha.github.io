201e832d345279dc63ede1309ec64d22dd445cd6
Yunes Noronha <yunes.silva@nextel.com.br>
2020-12-03 16:33:55 -0300

    portifolio

--- js/charts.js
+++ js/charts.js
@@ -6,12 +6,12 @@ var skillTechnologyChar = new Chart(document.getElementById("skillTechnology"), 
     "label": "Skill technology",
     "data": [8, 2, 10, 4, 2, 3, 8],
     "fill": false,
-    "backgroundColor": ["rgba(54, 162, 235, 0.2)", "rgba(54, 162, 235, 0.2)",
-    "rgba(54, 162, 235, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(54, 162, 235, 0.2)",
-    "rgba(54, 162, 235, 0.2)", "rgba(54, 162, 235, 0.2)"
+    "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)",
+    "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)",
+    "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"
     ],
-    "borderColor": ["rgb(54, 162, 235)", "rgb(54, 162, 235)", "rgb(54, 162, 235)",
-    "rgb(54, 162, 235)", "rgb(54, 162, 235)", "rgb(54, 162, 235)", "rgb(54, 162, 235)"
+    "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"
     ],
     "borderWidth": 1
     }]
@@ -26,6 +26,7 @@ var skillTechnologyChar = new Chart(document.getElementById("skillTechnology"), 
     }
     }
     });
+
 
     var personalSkillChar = new Chart(document.getElementById("personalSkill"), {
         "type": "horizontalBar",
