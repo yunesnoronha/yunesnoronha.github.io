3ecf4150b4f1b54ff99e2f97932dab5466124a3a
Yunes Noronha <yunesnoronha@gmail.coma>
2023-05-04 10:10:04 -0300

    Organize imports

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -80,7 +80,7 @@ const personalSkills = [
     level: 80,
   },
   {
-    name: 'Written communication',
+    name: 'Communication',
     level: 70,
   },
   {
@@ -192,12 +192,156 @@ const RenderSkills = ({ isMobile, skills }: { isMobile: boolean; skills: any }) 
         <Flex direction={'column'} sx={{ width: `${isMobile ? `300px` : `400px`}` }}>
           <Flex justify={'space-between'}>
             <Text size={'md'}>{skill.name}</Text>
-            <Text size={'md'}> {skill.level}</Text>
+            <Text size={'md'}> {skill.level}%</Text>
           </Flex>
           <Progress mt={0} value={skill.level} color={'cyan'} />
         </Flex>
       ))}
     </>
+  );
+};
+
+const RenderHome = ({ yearExperience }: { yearExperience: number }) => {
+  return (
+    <Stack align={'center'} mt={'md'}>
+      <Image
+        width={256}
+        radius={256}
+        sx={{ border: '3px solid #E9ECEF', borderRadius: 256 }}
+        src={process.env.PUBLIC_URL + 'img/yunes-cover.jpeg'}
+        alt="Yunes Noronha"
+      />
+      <Group>
+        <ActionIcon
+          color={'cyan'}
+          variant="subtle"
+          size={48}
+          component="a"
+          href={'https://www.linkedin.com/in/yunesnoronha/'}
+        >
+          <IconBrandLinkedin stroke={1.5} />
+        </ActionIcon>
+        <ActionIcon color={'cyan'} variant="subtle" size={48} component="a" href={'mailto:yunesnoronha@gmail.com'}>
+          <IconMailForward stroke={1.5} />
+        </ActionIcon>
+        <ActionIcon color={'cyan'} variant="subtle" size={48} component="a" href={'https://github.com/yunesnoronha'}>
+          <IconBrandGithub stroke={1.5} />
+        </ActionIcon>
+        <ActionIcon
+          color={'cyan'}
+          variant="subtle"
+          size={48}
+          component="a"
+          href={'https://api.whatsapp.com/send?phone=5513981949758'}
+        >
+          <IconBrandWhatsapp stroke={1.5} />
+        </ActionIcon>
+      </Group>
+      <Title order={1}>Hey, I´m Yunes Noronha</Title>
+      <Text size={'lg'} color={'dimmed'}>
+        Hi, I´m <b>Yunes Noronha</b> Fullstack Developer from <b>Brazil</b> with {yearExperience}+ years of experience.
+      </Text>
+      <Group>
+        <Button
+          color={'cyan'}
+          leftIcon={<IconDownload />}
+          component="a"
+          href={process.env.PUBLIC_URL + '/CV/CV.pdf'}
+          download
+        >
+          Download CV
+        </Button>
+        <Button color={'cyan'} leftIcon={<IconFileCertificate />} component={Link} to={'/CV'}>
+          CV Online
+        </Button>
+      </Group>
+    </Stack>
+  );
+};
+const RenderAbout = ({ yearExperience }: { yearExperience: number }) => {
+  return (
+    <Container mb={'md'}>
+      <Stack align={'center'} mt={'md'}>
+        <Title order={1}>Know Me More</Title>
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+          <Text size="lg" ta="justify">
+            I'm a Fullstack developer passionate about technology. I like to develop simple, clean and elegant websites
+            that provide real value to the end user. Simple is always the best. <br />I have {yearExperience}+ years of
+            experience with software development in various technologies on the market, great experience with web
+            development with React/Node js and database design. Great knowledge in other languages like C#, Python, Js,
+            Typescript etc. <br />
+            All this time I have seen major programming paradigm shifts as well as project management methodologies.
+          </Text>
+          <Stack align={'Center'} justify={'flex-start'}>
+            <Avatar alt="10" color="cyan" size={100} radius="xl">
+              {yearExperience}
+            </Avatar>
+            <Title order={3}>
+              Years of <b>Experience</b>
+            </Title>
+          </Stack>
+        </SimpleGrid>
+      </Stack>
+    </Container>
+  );
+};
+
+const RenderSummary = () => {
+  return (
+    <Container mb={'md'}>
+      <Stack align={'center'} mt={'md'}>
+        <Title order={1}>A summary of My Resume</Title>
+      </Stack>
+      <SimpleGrid cols={2} mt={'md'} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+        <Flex gap={'md'}>
+          <Divider size="xl" color={'cyan'} orientation="vertical" />
+          <Stack align={'flex-start'}>
+            <Title order={3}>My Education</Title>
+            <RenderEducation />
+          </Stack>
+        </Flex>
+        <Flex gap={'md'}>
+          <Divider size="xl" color={'cyan'} orientation="vertical" />
+          <Stack align={'flex-start'}>
+            <Title order={3}>My Experience</Title>
+            <RenderXp />
+          </Stack>
+        </Flex>
+      </SimpleGrid>
+    </Container>
+  );
+};
+const RenderPortfolio = () => {
+  return (
+    <Container mb={'md'}>
+      <Stack align={'center'} mt={'md'}>
+        <Title order={1}>Portfolio</Title>
+      </Stack>
+      <SimpleGrid cols={3} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+        <CardPortfolio
+          img={'img/port-site.png'}
+          p="xl"
+          radius={'xs'}
+          sx={{ boxShadow: '2px 2px 5px  #74717143' }}
+          children={undefined}
+          title="Site Pessoal"
+          text="Desenvolvido por Yunes Noronha, 2023."
+          link="https://yunesnoronha.com/"
+          linkText="Yunes Noronha"
+        />
+        <CardPortfolio
+          img={'img/port-gsf.png'}
+          p="xl"
+          radius={'xs'}
+          sx={{ boxShadow: '2px 2px 5px  #74717143' }}
+          children={undefined}
+          title="GSF Service"
+          text="Desenvolvido por Yunes Noronha e Andrey Naldoni, 2018."
+          link="https://gsfservice.com.br/"
+          linkText="GSF Service"
+        />
+      </SimpleGrid>
+    </Container>
   );
 };
 export default function Home() {
@@ -216,114 +360,13 @@ export default function Home() {
       })}
     >
       <Container bg={colorScheme === 'dark' ? 'dark' : '#F8F9FA'} mb={'lg'}>
-        <Stack align={'center'} mt={'md'}>
-          <Image
-            width={256}
-            radius={256}
-            sx={{ border: '3px solid #E9ECEF', borderRadius: 256 }}
-            src={process.env.PUBLIC_URL + 'img/yunes-cover.jpeg'}
-            alt="Yunes Noronha"
-          />
-          <Group>
-            <ActionIcon
-              color={'cyan'}
-              variant="subtle"
-              size={48}
-              component="a"
-              href={'https://www.linkedin.com/in/yunesnoronha/'}
-            >
-              <IconBrandLinkedin stroke={1.5} />
-            </ActionIcon>
-            <ActionIcon color={'cyan'} variant="subtle" size={48} component="a" href={'mailto:yunesnoronha@gmail.com'}>
-              <IconMailForward stroke={1.5} />
-            </ActionIcon>
-            <ActionIcon
-              color={'cyan'}
-              variant="subtle"
-              size={48}
-              component="a"
-              href={'https://github.com/yunesnoronha'}
-            >
-              <IconBrandGithub stroke={1.5} />
-            </ActionIcon>
-            <ActionIcon
-              color={'cyan'}
-              variant="subtle"
-              size={48}
-              component="a"
-              href={'https://api.whatsapp.com/send?phone=5513981949758'}
-            >
-              <IconBrandWhatsapp stroke={1.5} />
-            </ActionIcon>
-          </Group>
-          <Title order={1}>Hey, I´m Yunes Noronha</Title>
-          <Text size={'lg'} color={'dimmed'}>
-            Hi, I´m <b>Yunes Noronha</b> Fullstack Developer from <b>Brazil</b> with {yearExperience}+ years of
-            experience.
-          </Text>
-          <Group>
-            <Button
-              color={'cyan'}
-              leftIcon={<IconDownload />}
-              component="a"
-              href={process.env.PUBLIC_URL + '/CV/CV.pdf'}
-              download
-            >
-              Download CV
-            </Button>
-            <Button color={'cyan'} leftIcon={<IconFileCertificate />} component={Link} to={'/CV'}>
-              CV Online
-            </Button>
-          </Group>
-        </Stack>
+        <RenderHome yearExperience={yearExperience} />
       </Container>
       <Flex bg={colorScheme === 'dark' ? 'dark' : '#ffff'}>
-        <Container mb={'md'}>
-          <Stack align={'center'} mt={'md'}>
-            <Title order={1}>Know Me More</Title>
-            <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
-              <Text size="lg" ta="justify">
-                I'm a Fullstack developer passionate about technology. I like to develop simple, clean and elegant
-                websites that provide real value to the end user. Simple is always the best. <br />I have{' '}
-                {yearExperience}+ years of experience with software development in various technologies on the market,
-                great experience with web development with React/Node js and database design. Great knowledge in other
-                languages like C#, Python, Js, Typescript etc. <br />
-                All this time I have seen major programming paradigm shifts as well as project management methodologies.
-              </Text>
-              <Stack align={'Center'} justify={'flex-start'}>
-                <Avatar alt="10" color="cyan" size={100} radius="xl">
-                  {yearExperience}
-                </Avatar>
-                <Title order={3}>
-                  Years of <b>Experience</b>
-                </Title>
-              </Stack>
-            </SimpleGrid>
-          </Stack>
-        </Container>
+        <RenderAbout yearExperience={yearExperience} />
       </Flex>
       <Flex bg={colorScheme === 'dark' ? 'dark' : '#F8F9FA'} mb={'md'}>
-        <Container mb={'md'}>
-          <Stack align={'center'} mt={'md'}>
-            <Title order={1}>A summary of My Resume</Title>
-          </Stack>
-          <SimpleGrid cols={2} mt={'md'} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
-            <Flex gap={'md'}>
-              <Divider size="xl" color={'cyan'} orientation="vertical" />
-              <Stack align={'flex-start'}>
-                <Title order={3}>My Education</Title>
-                <RenderEducation />
-              </Stack>
-            </Flex>
-            <Flex gap={'md'}>
-              <Divider size="xl" color={'cyan'} orientation="vertical" />
-              <Stack align={'flex-start'}>
-                <Title order={3}>My Experience</Title>
-                <RenderXp />
-              </Stack>
-            </Flex>
-          </SimpleGrid>
-        </Container>
+        <RenderSummary />
       </Flex>
       <Flex bg={colorScheme === 'dark' ? 'dark' : '#ffff'} mb={'md'} p={'md'} wrap="wrap">
         <Container>
@@ -346,35 +389,7 @@ export default function Home() {
         </Container>
       </Flex>
       <Flex bg={colorScheme === 'dark' ? 'dark' : '#ffff '} gap="md">
-        <Container mb={'md'}>
-          <Stack align={'center'} mt={'md'}>
-            <Title order={1}>Portfolio</Title>
-          </Stack>
-          <SimpleGrid cols={3} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
-            <CardPortfolio
-              img={'img/port-site.png'}
-              p="xl"
-              radius={'xs'}
-              sx={{ boxShadow: '2px 2px 5px  #74717143' }}
-              children={undefined}
-              title="Site Pessoal"
-              text="Desenvolvido por Yunes Noronha, 2023."
-              link="https://yunesnoronha.com/"
-              linkText="Yunes Noronha"
-            />
-            <CardPortfolio
-              img={'img/port-gsf.png'}
-              p="xl"
-              radius={'xs'}
-              sx={{ boxShadow: '2px 2px 5px  #74717143' }}
-              children={undefined}
-              title="GSF Service"
-              text="Desenvolvido por Yunes Noronha e Andrey Naldoni, 2018."
-              link="https://gsfservice.com.br/"
-              linkText="GSF Service"
-            />
-          </SimpleGrid>
-        </Container>
+        <RenderPortfolio />
       </Flex>
     </AppShell>
   );
