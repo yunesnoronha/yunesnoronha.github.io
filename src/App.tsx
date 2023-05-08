0275569e0991b599517a4c738769d75b60f38838
Yunes Noronha <yunesnoronha@gmail.coma>
2023-05-08 14:16:15 -0300

    translate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import i18next from 'i18next';
 import { useState } from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import CV from './pages/CV';
 import Home from './pages/Home';
@@ -20,10 +22,12 @@ export default function App() {
         withGlobalStyles
         withNormalizeCSS
       >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/CV" element={<CV />} />
-        </Routes>
+        <I18nextProvider i18n={i18next}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/CV" element={<CV />} />
+          </Routes>
+        </I18nextProvider>
       </MantineProvider>
     </ColorSchemeProvider>
   );
